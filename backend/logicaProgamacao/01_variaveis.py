394c7836b9d3610c1669fdbfa37173f5768d1e90
Nicolas <luizcestaro@gmail.com>
2025-06-17 09:42:45 -0300

    vaidarocu

---
+++ backend/logicaProgamacao/01_variaveis.py
@@ -0,0 +1,9 @@
+nome = "Nicolas Miguel Cestaro"
+idade = 17
+altura = 1.78
+tem_carteira = False
+
+print("Nome:", nome)
+print("Idade:", idade)
+print("Altura:", altura)
+print("Tem carteira?:", tem_carteira)
---
+++ backend/logicaProgamacao/02_variaveis.py
@@ -0,0 +1,17 @@
+Nome   = "Nicolas Miguel"
+Sobrenome  = "Cestaro"
+Idade = 17
+Peso = 79
+Altura = 1.78
+Quanto_de_braco = 35.5
+tenho_carteira_de_motoriasta = False
+tenho_idade_para_viajar_sozinho = False
+
+print("Nome:", Nome)
+print("Sobrenome:", Sobrenome)
+print("Idade:", Idade)
+print("Peso:", Peso)
+print("Altura:", Altura)
+print("Quanto de braço?:", Quanto_de_braco)
+print("Tenho carteira de motorista ?:", tenho_carteira_de_motoriasta)
+print("Posso viajar sozinho ?:", tenho_idade_para_viajar_sozinho)
