c12e06eb6c188d2373609a6992acd0fbb87931c9
Nicolas <luizcestaro@gmail.com>
2025-06-23 09:02:06 -0300

    vaidarocu

---
+++ backend/logicaProgamacao/02_condicionais.py
@@ -0,0 +1,8 @@
+nota = int(input("digite sua nota:"))
+
+if (nota >= 80):
+    print("Parabens")
+elif (nota < 80 and nota > 60):
+    print ("pode melhorar")
+elif (nota < 60):
+    print ("recuperacao")
