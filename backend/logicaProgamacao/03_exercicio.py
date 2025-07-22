8c092a450e1278875714d533bb12dd1d4c0d57a0
Nicolas <luizcestaro@gmail.com>
2025-07-22 09:35:42 -0300

    vaidarocu

---
+++ backend/logicaProgamacao/03_exercicio.py
@@ -0,0 +1,8 @@
+valor1 = float(input("Digite o primeiro valor: "))
+valor2 = float(input("Digite o segundo valor: "))
+
+print("Soma:", valor1 + valor2)
+print("Subtração:", valor1 - valor2)
+print("Multiplicação:", valor1 * valor2)
+print("divisão:", valor1 / valor2)
+    
