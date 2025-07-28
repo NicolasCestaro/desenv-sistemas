a2b053c818645c91082b1e40493c270e2a9f9399
Nicolas <luizcestaro@gmail.com>
2025-07-28 08:48:20 -0300

    vaidarocu

---
+++ backend/logicaProgamacao/05_exercicio.py
@@ -0,0 +1,14 @@
+soma = 0
+for i in range(1, 21):
+    soma = soma + i
+print("Soma de 1 a 20:", soma)
+
+
+# soma2 = 0
+# numero = int(input("Digite um número (0 para parar): "))
+
+# while numero != 0:
+#     soma2 = soma2 + numero
+#     numero = int(input("Digite um número (0 para parar): "))
+
+# print("Soma dos números digitados:", soma2)
