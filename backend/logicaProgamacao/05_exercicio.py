06f9571a33123204c8e404ac398ce1261b16e4d1
Nicolas <luizcestaro@gmail.com>
2025-07-29 09:30:28 -0300

    vaidarocu

--- backend/logicaProgamacao/05_exercicio.py
+++ backend/logicaProgamacao/05_exercicio.py
@@ -1,7 +1,7 @@
-soma = 0
-for i in range(1, 21):
-    soma = soma + i
-print("Soma de 1 a 20:", soma)
+# soma = 0
+# for i in range(1, 21):
+#     soma = soma + i
+# print("Soma de 1 a 20:", soma)
 
 
 # soma2 = 0
@@ -12,3 +12,42 @@ print("Soma de 1 a 20:", soma)
 #     numero = int(input("Digite um número (0 para parar): "))
 
 # print("Soma dos números digitados:", soma2)
+
+
+
+# senha = input("Digite a senha: ")
+
+# while senha != senha_correta:
+#     print("Senha inválida")
+#     senha = input("Digite a senha: ")
+
+# print("Acesso permitido")
+
+
+# pedir nome e senha ao usuario 
+
+# mostrar "bem vindo" quando acertara senha e o nome 
+
+# apos pedir o salario do usuario 
+
+# mostrar salario anual 
+
+# se o salario anual for maior que 100mil mostrar mensagem "rico"
+
+nome = ""
+senha = ""
+
+while nome == "" or senha == "":
+    nome = input("Digite seu nome: ")
+    senha = input("Digite sua senha: ")
+
+print("Bem-vindo")
+
+salario = float(input("Digite seu salário mensal: "))
+salario_anual = salario * 12
+
+print("Seu salário anual é:", salario_anual)
+
+if salario_anual > 100000:
+    print("Rico")
+
