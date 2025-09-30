8ac295ec7a40071bc4606a688f61bec4fedfc361
Nicolas <luizcestaro@gmail.com>
2025-09-30 08:58:09 -0300

    30/09/2025

--- backend/logicaProgamacao/explicacao_json.py
+++ backend/logicaProgamacao/explicacao_json.py
@@ -5,21 +5,26 @@
 
 
 # #sempre importar a json
-# import json 
-# inventario = []
-# #lendo o arquivo json
-# try:
-#     with open("loja.json", "r") as arquivo:
-#         inventario = json.load(arquivo)
-# except FileNotFoundError:
-#     print("Arquivo não encontrado")
+import json 
+inventario = []
+#lendo o arquivo json
+try:
+    with open("loja.json", "r") as arquivo:
+        inventario = json.load(arquivo)
+except FileNotFoundError:
+    print("Arquivo não encontrado")
     
-    # try:
-    #     nome=input("digite o nome do produto: ")
-    #     quantidade=int(input("digite a quantidade: "))
-    #     preco=float(input("digite o preço: "))
-    #     except ValueError:
-    #         print("digite o valor corretamente")
+try:
+    nome=input("digite o nome do produto: ")
+    quantidade=int(input("digite a quantidade: "))
+    preco=float(input("digite o preço: "))
+    except ValueError:
+        print("digite o valor corretamente")
     
-    #montar o objeto
-    novo_produto = {"nome": nome, "quantidade": quantidade, "preco": preco, "em_estoque":quantidade > 0 #expressao verdadeira ou falsa}
+ #montar o objeto
+ novo_produto = {"nome": nome, "quantidade": quantidade, "preco": preco, "em_estoque":quantidade > 0 #expressao verdadeira ou falsa}                   
+# escrever o objeto no arquivo json
+inventario.append(novo_produto)
+with open("loja.json", "w") as arquivo:
+ json.dump(inventario, arquivo, indent=4) #indent=4 -> Formatar o arquivo json
+print("Produto adicionado com sucesso")
