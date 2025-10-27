8ee5688d6941ef23800e3804634f6ffa6733affb
Nicolas <luizcestaro@gmail.com>
2025-10-27 08:55:04 -0300

    27/10/2025

--- gerenciador_tarefas/config/tarefas/urls.py
+++ gerenciador_tarefas/config/tarefas/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-urlpatterns=[
-    path('',views.listar_tarefas,name='listar_tarefas'),
-    path('<int:tarefa_id>/', views.detalhe_tarefa, name='detalhe_tarefa'),
 
+urlpatterns = [
+    path('', views.listar_tarefas, name='listar_tarefas'),
+    path('<int:tarefa_id>/', views.detalhe_tarefa, name='detalhe_tarefa'),
+    path('adicionar/', views.adicionar_tarefa, name='adicionar_tarefa')
 ]
--- gerenciador_tarefas/config/tarefas/views.py
+++ gerenciador_tarefas/config/tarefas/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import get_object_or_404, redirect,render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Tarefa
 from django.http import HttpResponse
+
 def listar_tarefas(request):
-    tarefas_salvas=Tarefa.objects.all()
-    contexto={"minhas_tarefas":tarefas_salvas}
-    return render (request,'tarefas/lista.html',contexto)
+    tarefas_salvas = Tarefa.objects.all()
+    contexto = {"minhas_tarefas": tarefas_salvas}
+    return render(request, 'tarefas/lista.html', contexto)
     
 def detalhe_tarefa(request, tarefa_id):
     tarefa = get_object_or_404(Tarefa, pk=tarefa_id)
-    return render(request,'tarefas/detalhe.html',{'tarefa':tarefa})
+    return render(request, 'tarefas/detalhe.html', {'tarefa': tarefa})
+
+def adicionar_tarefa(request):
+    if request.method == 'POST':
+        titulo = request.POST.get('titulo')
+        descricao = request.POST.get('descricao')
+        Tarefa.objects.create(titulo=titulo, descricao=descricao)
+        return redirect('listar_tarefas')
+    return render(request, 'tarefas/form_tarefa.html')
